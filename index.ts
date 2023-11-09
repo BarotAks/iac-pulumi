1d68cca6391f87d50cb7f1630b7671d13a7d0801
Akshita <barot.ak@northeastern.edu>
2023-11-08 21:00:17 -0500

    adding iam

--- index.ts
+++ index.ts
@@ -9,7 +9,7 @@ const config = new pulumi.Config("myfirstpulumi");
 const awsConfig = new pulumi.Config("aws");
 
 
-// Get the AWS profile from the config
+
 // Get the AWS profile from the config
 const awsProfile = awsConfig.require("profile");
 
@@ -25,9 +25,23 @@ const subnetMask = config.require("subnetMask");
 const vpcCidrBlock = config.require("vpcCidrBlock");
 const amiId = config.require("amiId");
 const keyPair = config.require("keyPair");
+const rdsName = config.require("identifier");
+const intClass = config.require("instanceClass");
+const engVersion = config.require("engineVersion");
+const storageType = config.require("storageType");
+const eng = config.require("engine");
+const databaseName = config.require("dbName");
+const parameterGroupName = config.require("parameterGroupName");
+const dbUsername = config.requireSecret("dbUsername");
+const dbPassword = config.requireSecret("dbPassword");
+const domainName = config.require("domainName");
+const hostedZoneId = config.require("hostedZoneId");
+
+
 // Declare separate arrays for public and private subnets
 const publicSubnets: aws.ec2.Subnet[] = [];
 const privateSubnets: aws.ec2.Subnet[] = [];
+
 
 function calculateCIDR(vpcCidrBlock: string, subnetIndex: number,  totalSubnets: number): string {
     const cidrParts = vpcCidrBlock.split('/');
@@ -207,6 +221,59 @@ const appSecurityGroup = new aws.ec2.SecurityGroup("app-sg", {
     ],
 });
 
+// Create an EC2 security group for RDS instances
+const rdsSecurityGroup = new aws.ec2.SecurityGroup("rds-sg", {
+    vpcId: vpc.id,
+    description: "RDS Security Group",
+    ingress: [
+        // Allow MySQL/MariaDB (3306) traffic or PostgreSQL (5432) traffic from the application security group
+        {
+            protocol: "tcp",
+            fromPort: 3306,  
+            toPort: 3306,    
+            securityGroups: [appSecurityGroup.id]  // Only allows traffic from the application security group
+        }
+    ],
+    egress: [
+        // Restrict all outgoing internet traffic
+        {
+            protocol: "tcp",
+            fromPort: 0,
+            toPort: 0,
+            cidrBlocks: [publicCidrBlockName]
+            
+        }
+    ],
+}, { provider });
+
+// Define a simple IAM instance profile name
+// const iamInstanceProfileName = "MyInstanceProfile";
+
+const customRole = new aws.iam.Role("custom-role", {
+    name: "MyInstanceProfile", // Use the defined IAM instance profile name
+    assumeRolePolicy: JSON.stringify({
+        Version: "2012-10-17",
+        Statement: [{
+            Action: "sts:AssumeRole",
+            Effect: "Allow",
+            Principal: {
+                Service: "ec2.amazonaws.com"
+            }
+        }]
+    }),
+});
+
+// Attach the CloudWatchAgentServerPolicy managed policy
+const cloudWatchPolicyAttachment = new aws.iam.PolicyAttachment("cloudwatch-policy-attachment", {
+    roles: [customRole.name],
+    policyArn: "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy",
+});
+
+const iamInstanceProfile = new aws.iam.InstanceProfile("my-instance-profile", {
+    role: customRole.name,
+});
+
+
 // Export the IDs of the resources created
 export const vpcId = vpc.id;
 export const publicSubnetIds = subnets.apply(subnets => 
@@ -216,85 +283,122 @@ export const privateSubnetIds = subnets.apply(subnets =>
     subnets.filter((_, index) => index % 2 !== 0).map(subnet => subnet.id)
 );
 
-// Create RDS parameter group
-const dbParameterGroup = new aws.rds.ParameterGroup("db-parameter-group", {
-    family: "mariadb10.4", // Specify your database family
-    parameters: {
-        // Define your custom parameters here if necessary
-    },
-});
+const dbParameterGroup = new aws.rds.ParameterGroup(parameterGroupName, {
+    family: "mariadb10.5",
+    description: "Custom parameter group for mariadb10.5",
+    parameters: [{
+        name: "max_connections",
+        value: "100"
+    }]
+}, { provider });
 
-// Create RDS instance (MariaDB)
-const rdsInstance = new aws.rds.Instance("mydb", {
+// Creating a DB subnet group
+const dbSubnetGroup = new aws.rds.SubnetGroup("dbsubnetgrp", {
+    subnetIds: privateSubnetIds,
+    tags: {
+        Name: "dbsubnetgrp",
+    },
+}, { provider });
+
+// Create an RDS instance with MariaDB
+const dbInstance = new aws.rds.Instance("mydbinstance", {
+    instanceClass: intClass,
+    dbSubnetGroupName: dbSubnetGroup.name, 
+    parameterGroupName: dbParameterGroup.name, 
+    engine: eng,
+    engineVersion: engVersion, 
     allocatedStorage: 20,
-    storageType: "gp2",
-    engine: "mariadb",
-    engineVersion: "10.4", // Specify your desired MariaDB version
-    instanceClass: "db.t2.micro", // Use the appropriate instance class
-    name: "csye6225", // Database name
-    username: "csye6225",
-    password: "Pass1234", // Replace with a strong password
+    storageType: storageType,
+    username: dbUsername,
+    password: dbPassword,
     skipFinalSnapshot: true,
+    vpcSecurityGroupIds: [rdsSecurityGroup.id],
     publiclyAccessible: false,
-    vpcSecurityGroupIds: [appSecurityGroup.id],
-    allocatedStorage: 20,
-    storageType: "gp2",
-    dbSubnetGroupName: dbSubnetGroup.name,
-    skipFinalSnapshot: true,
-    parameterGroupName: dbParameterGroup.name,
+    identifier: rdsName,
+    dbName: databaseName
+
+}, { provider });
+
+const userData = pulumi.all([dbInstance.endpoint, dbUsername, dbPassword,databaseName]).apply(([endpoint, username, password,databaseName]) => {
+    const parts = endpoint.split(':');
+    const endpoint_host = parts[0];
+    const dbPort = parts[1];
+    
+    // Create the bash script string
+    return `#!/bin/bash
+ENV_FILE="/home/admin/webapp/.env"
+
+# Create or overwrite the environment file with the environment variables
+echo "DB_HOST=${endpoint_host}" > $ENV_FILE
+echo "DBPORT=${dbPort}" >> $ENV_FILE
+echo "DB_USERNAME=${username}" >> $ENV_FILE
+echo "DB_PASSWORD=${password}" >> $ENV_FILE
+echo "DB_DATABASE=${databaseName}" >> $ENV_FILE
+echo "CSV_PATH=/home/admin/webapp/users.csv" >> $ENV_FILE
+echo "PORT=3000" >> $ENV_FILE
+
+# Optionally, you can change the owner and group of the file if needed
+sudo chown admin:admin $ENV_FILE
+
+# Adjust the permissions of the environment file
+sudo chmod 600 $ENV_FILE
+
+# Fetch configurations using CloudWatch Agent
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+-a fetch-config \
+-m ec2 \
+-c file:/home/admin/webapp/packer/cloudwatch-config.json \
+-s
+
+# Restart the application service
+sudo systemctl restart webapp.service
+`;
 });
 
-// Security Group Rules: Allow EC2 to connect to MariaDB
-appSecurityGroup.createIngressRule("allow-db", {
-    type: "ingress",
-    fromPort: 3306,
-    toPort: 3306,
-    protocol: "tcp",
-    cidrBlocks: [aws.ec2.getPrivateIp({})], // Get private IP of EC2 instance
-});
 
-
-// Create an EC2 instance with User Data and Systemd Unit Setup
+// Create an EC2 instance
 const ec2Instance = new aws.ec2.Instance("web-app-instance", {
-    ami: "ami-00e5b41d2127eabf5",
+    ami: amiId,
     instanceType: "t2.micro",
-    vpcSecurityGroupIds: [appSecurityGroup.id],
-    subnetId: pulumi.output(publicSubnetIds[0]),
+    vpcSecurityGroupIds: [appSecurityGroup.id],  // attach application security group
+    subnetId: pulumi.output(publicSubnetIds[0]),  // specify one of the public subnets
     associatePublicIpAddress: true,
-    keyName: keyPair,
-    disableApiTermination: false,
+    keyName: keyPair, 
+    disableApiTermination: false,  // allows the instance to be terminated
     rootBlockDevice: {
-        deleteOnTermination: true,
-        volumeSize: 25,
-        volumeType: "gp2",
+        deleteOnTermination: true,  // ensure the EBS volume is deleted upon termination
+        volumeSize: 25, // set the root volume size to 25 GB
+        volumeType: "gp2", // set the root volume type to General Purpose SSD (GP2)
     },
-    userData: pulumi.interpolate `#!/bin/bash
-        export DB_HOSTNAME=${rdsInstance.endpoint};
-        export DB_USERNAME=csye6225;
-        export DB_PASSWORD=<YOUR_DB_PASSWORD>; // Replace with your MariaDB password
-        # Additional configuration and startup commands for your application
-        # ...
-        # Start your application here
-    `,
     tags: {
         Name: "web-app-instance",
     },
-}, { dependsOn: publicSubnets });
+    iamInstanceProfile: iamInstanceProfile.name, // Use the defined IAM instance profile name
+    userData: userData,
+}, { dependsOn: publicSubnets}); 
 
-// Systemd Unit Setup
-const systemdServiceUnit = new aws.ec2.CloudInit("systemd-service", {
-    instanceId: ec2Instance.id,
-    userDataBase64: ec2Instance.userData.apply(userData => 
-        Buffer.from(userData).toString("base64")),
+// Get the public IP address of the EC2 instance
+const ec2InstancePublicIp = ec2Instance.publicIp;
+
+// Create a Route53 Record Set for the domain
+const domainRecord = new aws.route53.Record(`${domainName}-record`, {
+    zoneId: hostedZoneId,
+    name: domainName,
+    type: "A",
+    ttl: 60,
+    records: [ec2InstancePublicIp],
 });
 
-// Output RDS endpoint and EC2 public IP
-export const rdsEndpoint = rdsInstance.endpoint;
-export const ec2PublicIp = ec2Instance.publicIp;
-
-// Export the security group ID, internet gateway ID, route table IDs, and public IP
+// Export the security group ID
 export const securityGroupId = appSecurityGroup.id;
 export const internetGatewayId = internetGateway.id;
 export const publicRouteTableId = publicRouteTable.id;
 export const privateRouteTableId = privateRouteTable.id;
+// Export the public IP of the instance
 export const publicIp = ec2Instance.publicIp;
+// Export the rds security group ID
+export const rdsSecurityGroupId = rdsSecurityGroup.id;
+// Export the DNS record information for reference
+export const domainRecordName = domainRecord.name;
+export const domainRecordType = domainRecord.type;
+export const domainRecordValue = domainRecord.records;
